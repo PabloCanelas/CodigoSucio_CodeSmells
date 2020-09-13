318ba7687b054083e8b8b460585ccd869dc43f0c
Pablo Canelas <pablo.canelas.schlink@gmail.com>
2020-09-13 17:54:47 -0400

    Calculadora implementada

--- CodigoSucio/main.cpp
+++ CodigoSucio/main.cpp
@@ -7,9 +7,88 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+float Calculadora(float x, float y, int operacion){
+   cout<<"__________________"<<endl;
+    cout<<"Ultima operacion"<<endl;
+
+
+    if(operacion==1){
+        cout<<x<<"+"<<y<<"="<<x+y<<endl;
+        x=x+y;
+    }
+    if(operacion==2){
+        cout<<x<<"-"<<y<<"="<<x-y<<endl;
+        x=x-y;
+    }
+    if(operacion==3){
+        cout<<x<<"/"<<y<<"="<<x/y<<endl;
+        x=x/y;
+    }
+    if(operacion==4){
+        cout<<x<<"x"<<y<<"="<<x*y<<endl;
+        x=x*y;
+    }
+    
+    return x;
+}
+int main() {
+    int control=0;
+    float res=0;
+    int menu=-1;
+    float x,y;
+    if(control==0){
+        cout<<"Calculadora "<<endl;
+        cout<<"__________________"<<endl;
+
+        cout<<"Operaciones: "<<endl;
+       cout<<"1.- Sumar"<<endl;
+       cout<<"2.- Restar"<<endl;
+       cout<<"3.- Dividir"<<endl;
+       cout<<"4.- Multiplicar"<<endl;
+        cout<<"__________________"<<endl;
+       cout<<"Valor 1?"<<endl;
+       cin>>x;
+       cout<<"Valor 2?"<<endl;
+       cin>>y;
+       cout<<"operacion?"<<endl;
+       cin>>menu;
+       res=Calculadora(x, y, menu);
+        control++;
+    }
+    
+    if(control!=0)
+    {
+        while(menu!=5&&menu!=6){
+        cout<<"__________________"<<endl;
+        cout<<"Operaciones: "<<endl;
+        cout<<"1.- Sumar al valor actual"<<endl;
+        cout<<"2.- Restar al valor actual"<<endl;
+        cout<<"3.- Dividir al valor actual"<<endl;
+        cout<<"4.- Multiplicar al valor actual"<<endl;
+        cout<<"5.- Salir de la calculadora "<<endl;
+        cout<<"6.- Reiniciar la calculadora "<<endl;
+            
+        cout<<"__________________"<<endl;
+        cout<<"Valor actual: "<<res<<endl;
+        cout<<"__________________"<<endl;
+        cout<<"operacion?"<<endl;
+           
+        cin>>menu;
+        if(menu!=5&&menu!=6)
+        {
+        cout<<"Valor 2?"<<endl;
+              cin>>y;
+        res=Calculadora(res, y, menu);
+        }
+        }
+    }
+    
+    if(menu==6) main();
+    
+    
+    
     return 0;
 }
+/Users/pablocanelas
