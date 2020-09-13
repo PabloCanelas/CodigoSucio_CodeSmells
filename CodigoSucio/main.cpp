97647b9e4c793d5c7cd2b4d7939f75722f990e4b
Pablo Canelas <pablo.canelas.schlink@gmail.com>
2020-09-13 11:36:37 -0400

    Initial Commit

---
+++ CodigoSucio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CodigoSucio
+//
+//  Created by Pablo Canelas Schlink on 9/13/20.
+//  Copyright © 2020 Pablo Canelas Schlink. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
